135f288d88d29177aa917018961eea8ebf124ef0
Fabian Dörr <faflfama@outlook.com>
2017-05-05 11:22:10 +0200

    Projektdateien hinzufügen.

---
+++ SetLastUsername/main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace SetLastUsername
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChange = new System.Windows.Forms.Button();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.cbUsernames = new System.Windows.Forms.ComboBox();
+            this.cbLR = new System.Windows.Forms.CheckBox();
+            this.txtDomain = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(99, 52);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(78, 23);
+            this.btnChange.TabIndex = 0;
+            this.btnChange.Text = "OK";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(74, 9);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(75, 13);
+            this.lblUsername.TabIndex = 1;
+            this.lblUsername.Text = "Benutzername";
+            // 
+            // cbUsernames
+            // 
+            this.cbUsernames.FormattingEnabled = true;
+            this.cbUsernames.Location = new System.Drawing.Point(77, 25);
+            this.cbUsernames.Name = "cbUsernames";
+            this.cbUsernames.Size = new System.Drawing.Size(121, 21);
+            this.cbUsernames.TabIndex = 4;
+            // 
+            // cbLR
+            // 
+            this.cbLR.AutoSize = true;
+            this.cbLR.Location = new System.Drawing.Point(11, 27);
+            this.cbLR.Name = "cbLR";
+            this.cbLR.Size = new System.Drawing.Size(15, 14);
+            this.cbLR.TabIndex = 5;
+            this.cbLR.UseVisualStyleBackColor = true;
+            // 
+            // txtDomain
+            // 
+            this.txtDomain.Location = new System.Drawing.Point(32, 25);
+            this.txtDomain.Name = "txtDomain";
+            this.txtDomain.Size = new System.Drawing.Size(22, 20);
+            this.txtDomain.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "\\";
+            // 
+            // main
+            // 
+            this.AcceptButton = this.btnChange;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 86);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDomain);
+            this.Controls.Add(this.cbLR);
+            this.Controls.Add(this.cbUsernames);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.btnChange);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "main";
+            this.Text = "Benutzernamen setzen";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.main_FormClosing);
+            this.Load += new System.EventHandler(this.main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.ComboBox cbUsernames;
+        private System.Windows.Forms.CheckBox cbLR;
+        private System.Windows.Forms.TextBox txtDomain;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SetLastUsername/main.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Security.Principal;
+using System.IO;
+
+namespace SetLastUsername
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            bool adminRights;
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            adminRights = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+
+            if(adminRights)
+            {
+                string username;
+                if(cbLR.Checked && txtDomain.Text != "")
+                {
+                    username = txtDomain.Text + "\\" + cbUsernames.Text;
+                }
+                else
+                {
+                    username = cbUsernames.Text;
+                }
+                
+
+                Microsoft.Win32.RegistryKey key;
+                key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Authentication\\LogonUI", true);
+                key.SetValue("LastLoggedOnSAMUser", username);
+                key.SetValue("LastLoggedOnUser", username);
+                key.SetValue("LastLoggedOnDisplayName", "");
+
+                string lastSam;
+                string lastUser;
+
+                lastSam = Convert.ToString(key.GetValue("LastLoggedOnSAMUser"));
+                lastUser = Convert.ToString(key.GetValue("LastLoggedOnUser"));
+
+                if (lastSam == username && lastUser == username)
+                {
+                    MessageBox.Show("Der Benutzer " + username + " wurde erfolgreich eingetragen");
+                    this.Close();
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bitte als Administrator ausführen");
+            }       
+        }
+
+        private void main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("Programm beenden?", "Beenden", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                // cancel the closure of the form.
+                e.Cancel = true;
+            }
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            string[] files = Directory.GetDirectories(@"C:\\Users\");
+            foreach (string item in files)
+            {
+                FileInfo f = new FileInfo(item);
+                cbUsernames.Items.Add(f.Name);
+            }
+
+            cbUsernames.Items.Remove("Administrator");
+            cbUsernames.Items.Remove("All Users");
+            cbUsernames.Items.Remove("Default");
+            cbUsernames.Items.Remove("Default User");
+            cbUsernames.Items.Remove("Public");
+        }
+    }
+}
